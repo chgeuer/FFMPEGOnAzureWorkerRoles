cd86614a0196324b27cdb38eb0755e119fe7adbd
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-07-18 07:51:45 -0400

    a

--- FFMPEGLib/VideoConversionJobResult.cs
+++ FFMPEGLib/VideoConversionJobResult.cs
@@ -1,15 +1,10 @@
 ﻿namespace FFMPEGLib
 {
     using Newtonsoft.Json;
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class VideoConversionJobResults
     {
-
         public VideoConversionJobResults()
         {
             Results = new List<VideoConversionJobResult>();
@@ -33,10 +28,11 @@
     public class VideoConversionJobResult
     {
         public string CommandLine { get; set; }
-        public int ErrorCode { get; set; }
 
         public string StandardOut { get; set; }
 
         public string StandardErr { get; set; }
+
+        public int ErrorCode { get; set; }
     }
 }
